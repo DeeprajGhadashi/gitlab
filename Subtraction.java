d20589efd13832d0ed4e052ec257b2149353eed3
DeepG@123 <deeprajghadshi1920@gamil.com>
2023-05-05 20:06:34 +0530

    first commit

---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+public class Subtraction {
+
+	public static void main(String[] args) {
+	int a=10;
+	int b= 20;
+    int c=a-b;
+    int d=b-a;
+System.out.println("substraction of c is="+c);
+System.out.println("substraction of c is="+d);
+	}
+ 
+}
